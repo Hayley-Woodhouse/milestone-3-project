c115a7dd70950bbf1e4bbb385ac363dbb1bf028e
Hayley-Woodhouse <h.woodhouse639@gmail.com>
2022-07-18 20:34:55 +0000

    initial commit

---
+++ dw_fan_site/static/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // https://materializecss.com/navbar.html#! mobile menu
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
+
+  
