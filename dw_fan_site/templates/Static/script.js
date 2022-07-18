51f372a63ec4efeee3772833eb9c441751cef789
Hayley-Woodhouse <h.woodhouse639@gmail.com>
2022-07-18 20:29:22 +0000

    added nav and footer customized links to social media and site pages

--- dw_fan_site/templates/Static/script.js
+++
@@ -1,7 +0,0 @@
-/*-- https://materializecss.com/navbar.html#! mobile menu */
-document.addEventListener('DOMContentLoaded', function() {
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
-  });
-
-  
