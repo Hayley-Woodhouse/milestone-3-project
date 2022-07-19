bd409f70b39b346a901198d725561c54ece7b215
Hayley-Woodhouse <h.woodhouse639@gmail.com>
2022-07-19 13:32:16 +0000

    addapted variable name

--- dw_fan_site/routes.py
+++ dw_fan_site/routes.py
@@ -10,13 +10,14 @@ def home():
 
 @app.route("/books")  
 def books():
-    return render_template("books.html") 
+    books = list(Books.query.order_by(Books.book_name).all())
+    return render_template("books.html", books=books) 
 
 
 @app.route("/add_book", methods=["GET", "POST"])
 def add_book():
     if request.method == "POST":
-        books = Books(book_name=request.form.get("book_name"))
+        a_books = Books(book_name=request.form.get("book_name"))
         db.session.add(books)
         db.session.commit()
         return redirect(url_for("books"))
