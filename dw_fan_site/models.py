f0fe76d09f0f26ff5198b5f4a9614075cfe9f5b8
Hayley-Woodhouse <h.woodhouse639@gmail.com>
2022-07-20 21:23:47 +0000

    updated unique type to allow for repeatable data

--- dw_fan_site/models.py
+++ dw_fan_site/models.py
@@ -5,8 +5,8 @@ class Books(db.Model):
     # schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
     book_name = db.Column(db.String(25), unique=True, nullable=False)
-    author_name = db.Column(db.String(25), unique=True, nullable=False)
-    illustrator_name = db.Column(db.String(25), unique=True, nullable=False)
+    author_name = db.Column(db.String(25), unique=False, nullable=False)
+    illustrator_name = db.Column(db.String(25), unique=False, nullable=False)
     publication_date = db.Column(db.Integer, nullable=False)
     synopsis_info = db.Column(db.Text, nullable=False)
 
