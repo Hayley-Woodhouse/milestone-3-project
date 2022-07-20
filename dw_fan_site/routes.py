113435490ec3e42154d3e5af8240347f6c38eadb
Hayley-Woodhouse <h.woodhouse639@gmail.com>
2022-07-20 21:25:12 +0000

    added routes for update and delete crud book functionality

--- dw_fan_site/routes.py
+++ dw_fan_site/routes.py
@@ -24,9 +24,32 @@ def add_book():
             illustrator_name=request.form.get("illustrator_name"),
             publication_date=request.form.get("publication_date"),
             synopsis_info=request.form.get("synopsis_info"),
-            # books_id=request.form.get("books_id")
+            id=request.form.get("book_id")
         )
         db.session.add(book)
         db.session.commit()
         return redirect(url_for("books"))
     return render_template("add_book.html", books=books)
+
+
+@app.route("/edit_book/<int:book_id>", methods=["GET", "POST"])
+def edit_book(book_id):
+    book = Books.query.get_or_404(book_id)
+    if request.method == "POST":
+        book.id = book_id
+        book.book_name = request.form.get("book_name"),
+        book.author_name = request.form.get("author_name"),
+        book.illustrator_name = request.form.get("illustrator_name"),
+        book.publication_date = request.form.get("publication_date"),
+        book.synopsis_info = request.form.get("synopsis_info"),
+        db.session.commit()
+        return redirect(url_for("books"))
+    return render_template("edit_book.html", book=book)
+
+
+@app.route("/delete_book/<int:book_id>")
+def delete_book(book_id):
+    booked = Books.query.get_or_404(book_id)
+    db.session.delete(booked)
+    db.session.commit()
+    return redirect(url_for("books"))
