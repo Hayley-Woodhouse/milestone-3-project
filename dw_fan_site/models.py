b58aa44c6d00de5f85993e4f373b6bd9da77c5c4
Hayley-Woodhouse <h.woodhouse639@gmail.com>
2022-08-04 11:52:46 +0000

    expanded password string value to accomidate hashing

--- dw_fan_site/models.py
+++ dw_fan_site/models.py
@@ -13,3 +13,19 @@ class Books(db.Model):
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
         return f"#{self.id} - {self.book_name} - Details:{self.author_name} - date:{self.publication_date}"
+
+
+class Users(db.Model):
+    # schema for the sign_up model
+    id = db.Column(db.Integer, primary_key=True)
+    f_name = db.Column(db.String(15), unique=False, nullable=False)
+    l_name = db.Column(db.String(25), unique=False, nullable=False)
+    email_user = db.Column(db.String(50), unique=True, nullable=False)
+    password_user = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return f"#{self.id} - First Name:{self.f_name} - Last Name:{self.l_name}"
+
+
+
