3a2a2d3a0271808b26190a4c192d2de333cd9dba
Hayley-Woodhouse <h.woodhouse639@gmail.com>
2022-08-04 11:51:22 +0000

    added sign up route to add user to the db| added defensive programming to prevent duplicte email users and password match

--- dw_fan_site/routes.py
+++ dw_fan_site/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template, request, redirect, url_for
+from flask import flash, render_template, request, redirect, url_for
 from dw_fan_site import app, db
-from dw_fan_site.models import Books
+from dw_fan_site.models import Books, Users
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 @app.route("/")
@@ -31,9 +32,68 @@ def add_book():
         return redirect(url_for("books"))
     return render_template("add_book.html", books=books)
 
-@app.route("/sign_up")  
+
+@app.route("/sign_up", methods=["GET", "POST"])  
 def sign_up():
-    return render_template("sign_up.html") 
+    if request.method == "POST":
+        email_user=request.form.get("email_user")
+        password1=request.form.get("password_user")
+        password2=request.form.get("password2")
+    
+        # check if username already exists in db
+        email = Users.query.filter_by(email_user=email_user).first()
+        if email:
+            flash("email already used!")
+            return redirect(url_for("sign_up"))
+
+        # check if passwords match
+        elif password1 != password2:
+            flash("passwords do no match!")
+            return redirect(url_for("sign_up"))
+        else:
+            users = Users(
+                f_name=request.form.get("f_name"),
+                l_name=request.form.get("l_name"),
+                email_user=email_user,
+                password_user=generate_password_hash(password1, method='sha256'),
+                id=request.form.get("users_id")
+                )
+            db.session.add(users)
+            db.session.commit()
+            flash("Sign-up was Successful!")
+            return redirect("login")
+    return render_template("sign_up.html")
+
+
+
+    # if existing_user:
+    #     flash("This user already exists")
+    #     return redirect(url_for("sign_up"))
+
+    # return render_template("sign_up.html") 
+
+
+# @app.route("/register", methods=["GET", "POST"])
+# def register():
+#     if request.method == "POST":
+        # check if username already exists in db
+        # existing_user = mongo.db.users.find_one(
+        #     {"username": request.form.get("username").lower()})
+
+        # if existing_user:
+        #     flash("Username already exists")
+        #     return redirect(url_for("register"))
+
+        # register = {
+        #     "username": request.form.get("username").lower(),
+        #     "password": generate_password_hash(request.form.get("password"))
+        # }
+        # mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+    #     session["user"] = request.form.get("username").lower()
+    #     flash("Registration Successful!")
+    # return render_template("register.html")
 
 @app.route("/login")  
 def login():
