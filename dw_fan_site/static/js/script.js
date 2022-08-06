71f336fbfe64a8e95dd9b5c6467dc019ab8193a3
Hayley-Woodhouse <h.woodhouse639@gmail.com>
2022-08-06 15:31:52 +0000

    added style to comment section and post button

--- dw_fan_site/static/js/script.js
+++ dw_fan_site/static/js/script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     M.Sidenav.init(sidenav);
   });
 
-  // https://www.w3schools.com/howto/howto_js_toggle_password.asp
+  // toggle to view hiden password https://www.w3schools.com/howto/howto_js_toggle_password.asp
   function showPasswords() {
     let x = document.getElementById("password_user");
     let y = document.getElementById("password2");
@@ -19,4 +19,16 @@ document.addEventListener('DOMContentLoaded', function() {
       y.type = "password";
     }
   }
+
+ // comment section for books.html https://linuxhint.com/create-comment-box-using-html-css-javascript/
+const post= document.getElementById("post");
+post.addEventListener("click", function(){
+    let commentBoxValue= document.getElementById("comment_box").value;
+ 
+    let li = document.createElement("li");
+    let text = document.createTextNode(commentBoxValue);
+    li.appendChild(text);
+    document.getElementById("unordered").appendChild(li);
+ 
+});
     
