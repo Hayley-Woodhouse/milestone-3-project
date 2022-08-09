d6604678fc2c80de966e4299ab8156ce5f7a996a
Hayley-Woodhouse <h.woodhouse639@gmail.com>
2022-08-09 12:57:04 +0000

    removed unused function

--- dw_fan_site/static/js/script.js
+++ dw_fan_site/static/js/script.js
@@ -21,14 +21,14 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
  // comment section for books.html https://linuxhint.com/create-comment-box-using-html-css-javascript/
-const post= document.getElementById("post");
-post.addEventListener("click", function(){
-    let commentBoxValue= document.getElementById("comment_box").value;
+// const post= document.getElementById("post");
+// post.addEventListener("click", function(){
+//     let commentBoxValue= document.getElementById("comment_box").value;
+    
+//     let li = document.createElement("li");
+//     let text = document.createTextNode(commentBoxValue);
+//     li.appendChild(text);
+//     document.getElementById("unordered").appendChild(li);
  
-    let li = document.createElement("li");
-    let text = document.createTextNode(commentBoxValue);
-    li.appendChild(text);
-    document.getElementById("unordered").appendChild(li);
- 
-});
+// });
     
