272a2b9455afcf3ed6d75bfa08b2b3443ecd599b
Hayley-Woodhouse <h.woodhouse639@gmail.com>
2022-08-09 22:07:03 +0000

    added cascade to books and user model relationships

--- dw_fan_site/models.py
+++ dw_fan_site/models.py
@@ -10,7 +10,7 @@ class Books(db.Model):
     publication_date = db.Column(db.Integer, nullable=False)
     synopsis_info = db.Column(db.Text, nullable=False)
     # comments can have many posts
-    comment = db.relationship('Comment', backref='Commenters')
+    comment = db.relationship('Comment', backref='books', cascade='all, delete')
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
@@ -24,7 +24,8 @@ class Users(db.Model):
     l_name = db.Column(db.String(25), unique=False, nullable=False)
     email_user = db.Column(db.String(50), unique=True, nullable=False)
     password_user = db.Column(db.String(200), nullable=False)
-    users = db.relationship('Comment', backref='users')
+    # comments can have many posts
+    users = db.relationship('Comment', backref='users', cascade='all, delete')
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
@@ -36,23 +37,10 @@ class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     comment = db.Column(db.String(1000), unique=False, nullable=False)
     # Foreign key to link books (refer to primary key to the book)
-    commenters_id = db.Column(db.Integer, db.ForeignKey('books.id'))
-    
-    
-    
-    
+    books_id = db.Column(db.Integer, db.ForeignKey('books.id'))
+    # Foreign key to link users (refer to primary key to the users)
     users_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
         return f"#{self.id} - Comment:{self.comment}"
-
-
-class Images(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    images = db.Column(db.String(60), unique=True, nullable=False)
-
-    def __repr__(self):
-        # __repr__ to represent itself in the form of a string
-        return f"#{self.id} - Comment:{self.images}"
-
