71f5be041a3fac25fa3d0eb5cfdfbde0bcd4a510
Hayley-Woodhouse <h.woodhouse639@gmail.com>
2022-08-09 22:11:57 +0000

    tided up page| final commit

--- dw_fan_site/static/js/script.js
+++ dw_fan_site/static/js/script.js
@@ -19,16 +19,4 @@ document.addEventListener('DOMContentLoaded', function() {
       y.type = "password";
     }
   }
-
- // comment section for books.html https://linuxhint.com/create-comment-box-using-html-css-javascript/
-// const post= document.getElementById("post");
-// post.addEventListener("click", function(){
-//     let commentBoxValue= document.getElementById("comment_box").value;
-    
-//     let li = document.createElement("li");
-//     let text = document.createTextNode(commentBoxValue);
-//     li.appendChild(text);
-//     document.getElementById("unordered").appendChild(li);
- 
-// });
     
